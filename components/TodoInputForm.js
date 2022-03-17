ad7081e1fcaae31999da409d142c572b3e436fc8
POOJITA09 <salonigupta2001.sg@gmail.com>
2022-03-17 13:08:42 +0530

    input form added

--- components/TodoInputForm.js
+++ components/TodoInputForm.js
@@ -1,14 +1,56 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet,Button,TextInput,View,SafeAreaView,Alert} from 'react-native';
+import React from 'react';
 
 const TodoInputForm = () => {
+
+  const [text,onChangeText] = React.useState("");
+ 
+
   return (
-    <View>
-      <Text>TodoInputForm</Text>
-    </View>
-  )
-}
+      <SafeAreaView>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+          placeholder= 'Search here!'
+        />
+        <View style={styles.Button}>
+        <Button 
+        title="Submit"
+        color='green'
+        onPress={() => Alert.alert('Done')}
+      />
+      </View>
+
+         
+      </SafeAreaView>
+    );
+  };
+  
+  
+
 
 export default TodoInputForm
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  input: {
+    
+    height: 60,
+    margin: 25,
+    borderWidth: 5,
+    borderRadius:10,
+    padding:10,
+    borderColor:'blue',
+    fontSize:17,
+
+  },
+  Button:{
+    width:'50%',
+    paddingLeft:50,
+    marginLeft:70,
+    backgroundColor: 'white',
+    
+   
+
+  },
+});
