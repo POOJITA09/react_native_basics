1683093baea06a8e25bab98ae383f11d8cd1f0e7
Hemanth Kumar <pusupalahemanthkumar@gmail.com>
2022-03-17 15:31:10 +0530

    added functionality

--- components/TodoList.js
+++ components/TodoList.js
@@ -2,25 +2,12 @@ import { View, Text, ScrollView, FlatList, StyleSheet } from "react-native";
 import React, { useState } from "react";
 import { AntDesign } from "@expo/vector-icons";
 
-const TodoList = () => {
-  const [Items, setItems] = useState([
-    { key: 1, item: "Item 1" },
-    { key: 2, item: "Item 2" },
-    { key: 3, item: "Item 3" },
-    { key: 4, item: "Item 4" },
-    { key: 5, item: "Item 5" },
-    { key: 6, item: "Item 6" },
-    { key: 7, item: "Item 7" },
-    { key: 8, item: "Item 8" },
-    { key: 9, item: "Item 9" },
-    { key: 10, item: "Item 10" },
-    { key: 11, item: "Item 11" },
-  ]);
+const TodoList = ({ todosList }) => {
   return (
     <FlatList
       contentContainerStyle={styles.container}
       keyExtractor={(item) => item.key.toString()}
-      data={Items}
+      data={todosList}
       renderItem={({ item }) => (
         <View style={styles.item}>
           <Text style={styles.text}>{item.item}</Text>
@@ -33,14 +20,14 @@ const TodoList = () => {
 
 const styles = StyleSheet.create({
   container: {
-    padding:10,
+    padding: 10,
   },
   item: {
     margin: 10,
     paddingHorizontal: 25,
     paddingVertical: 12,
     backgroundColor: "#f4f4f4",
-    flexDirection:'row',
+    flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
   },
